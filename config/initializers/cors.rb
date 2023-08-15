be574e23d9e66ecf1e7f87eecc3bb61413401a68
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-15 15:19:21 -0500

    setup cors to manage permission

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
 #   allow do
 #     origins "example.com"
 #
@@ -14,3 +14,10 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3000' # Add any other origins as needed
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
